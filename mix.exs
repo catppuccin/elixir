38b80908ff47ce3848cfda86d59e676975a311b0
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2024-07-01 20:34:32 +0200

    deps: add credo

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Catppuccin.MixProject do
   defp deps do
     [
       {:jason, "~> 1.4"},
-      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 end
